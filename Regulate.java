6b26b7b68fcf668baf3c3f1dad2857d2fba01a7b
Corey Lazenby <45216719+TheCodeGhost@users.noreply.github.com>
2018-12-14 11:47:52 -0700

    Add files via upload

---
+++ Regulate.java
@@ -0,0 +1,88 @@
+public class Regulate
+{
+/*This is a program that displays the lyrics to "Regulate" */
+
+    public static void main(String[]args)
+    {
+        System.out.println("It was a clear black night, a clear white moon\n" +
+                "Warren G. is on the streets, trying to consume\n" +
+                "Some skirts for the eve, so I can get some funk\n" +
+                "Just rollin' in my ride, chillin all alone\n" +
+                "Just hit the eatside of the L.B.C.\n" +
+                "On a mission trying to find Mr. Warren G.\n" +
+                "Seen a car full of skirts ain't no need to tweak\n" +
+                "All you skirts know what's up with 213\n" +
+                "So I hooks a left on the 21 to Lewis\n" +
+                "Some brothas shootin dice so I said \"let's do this\"\n" +
+                "I jumped out the ride, and said\"what's up?\"\n" +
+                "Some brothas pulled some gats so I said \"I'm stuck\"\n" +
+                "Since these girls peepin me I'ma glide and swerve\n" +
+                "These hookers lookin so hard they straight hit the curb\n" +
+                "Gonna think of better things than some horny tricks\n" +
+                "I see my homey and some suckers all in his mix\n" +
+                "I'm gettin jacked, I'm breakin myself\n" +
+                "I can't believe they're taking Warren's wealth\n" +
+                "They took my rings, they took my rolex\n" +
+                "I looked at the brothas and said \"damn, what's next?\"\n" +
+                "They got my homey hemmed up and they all around\n" +
+                "Ain't none of them seeing if they going straight pound for pound\n" +
+                "I gotta come up real quick before they start to clown\n" +
+                "I besta pull out my strap and lay them busters down\n" +
+                "They got guns to my head\n" +
+                "I think I'm going down\n" +
+                "I can't believe this happened in my home town\n" +
+                "If I had wings I would fly\n" +
+                "Let me contemplate\n" +
+                "I glanced in the cut and I see my homey Nate\n" +
+                "Sixteen in the clip and one in the hole\n" +
+                "Nate Dogg is about to make some bodies turn cold\n" +
+                "Now they droppin and yellin\n" +
+                "It's a tad bit late\n" +
+                "Nate Dogg and Warren G. had to regulate\n" +
+                "I laid all them busters down\n" +
+                "I let my gat explode\n" +
+                "Now I'm switching my mind back into freak mode\n" +
+                "If you want skirts step back and observe\n" +
+                "I just left a gang of those over there on the curb\n" +
+                "Now Nate got the freaks\n" +
+                "And that's a known fact\n" +
+                "Before I got jacked I was on the same track\n" +
+                "Back up back up 'cause it's on\n" +
+                "N-A-T-E and me\n" +
+                "The Warren to the G\n" +
+                "Just like I thought\n" +
+                "They were in the same spot\n" +
+                "In need of some desperate help\n" +
+                "The Nate Dogg and the G-child\n" +
+                "Were in need of something else\n" +
+                "One of them dames was sexy as hell\n" +
+                "I said \"ooh I like your size\"\n" +
+                "She said \"my car's broke down and you seem real nice, \"\n" +
+                "\"would you let me ride?\"\n" +
+                "I got a car full of girls and it's going real swell\n" +
+                "The next stop is the east side motel\n" +
+                "I'm tweaking\n" +
+                "Onto a whole new level\n" +
+                "G-Funk\n" +
+                "Stept towards\n" +
+                "I dare ya\n" +
+                "Funk\n" +
+                "On a whole new level\n" +
+                "The rythmn is the base and the base is the treble\n" +
+                "Chords\n" +
+                "Strings\n" +
+                "We brings\n" +
+                "Melody\n" +
+                "G-Funk\n" +
+                "Where rythmn is life\n" +
+                "And life is rythmn\n" +
+                "If you know like I know\n" +
+                "You don't want to step to this\n" +
+                "It's the G-Funk era\n" +
+                "Funked out with a gangster twist\n" +
+                "If you smoke like I smoke\n" +
+                "Then you high like everyday\n" +
+                "And if your ass is a buster\n" +
+                "213 will regulate");
+    }
+}
